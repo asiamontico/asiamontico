3fc70cd19771bd26e6108ec993f58a42a160c1af
asiamontico <montico.1966494@studenti.uniroma1.it>
2024-10-14 00:23:30 +0200

    Add files via upload

--- scripts.py
+++ scripts.py
@@ -1101,3 +1101,178 @@ def merge_the_tools(input_string, length):
         print(unique_chars)
 
 
+# Number Line Jumps
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+def kangaroo(x1, v1, x2, v2):
+    if v1 == v2:
+        if x1 == x2:
+            return "YES"
+        else:
+            return "NO"
+    else:
+        n = (x2 - x1) / (v1 - v2)
+        return "YES" if n >= 0 and n.is_integer() else "NO"
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    x1 = int(first_multiple_input[0])
+    v1 = int(first_multiple_input[1])
+    x2 = int(first_multiple_input[2])
+    v2 = int(first_multiple_input[3])
+    result = kangaroo(x1, v1, x2, v2)
+    fptr.write(result + '\n')
+    fptr.close()
+
+# Viral Advertising
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+def viralAdvertising(n):
+    tot_liked = 0
+    received = 5
+    for i in range(n):
+        liked = received // 2 
+        tot_liked += liked    
+        received = liked*3
+    return tot_liked    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input().strip())
+    result = viralAdvertising(n)
+    fptr.write(str(result) + '\n')
+    fptr.close()
+
+# Recursive Digit Sum
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+def superDigit(n, k):
+    digit_sum = sum(int(d) for d in n)
+    total = digit_sum * k
+    
+    while total >= 10:
+        total = sum(int(d) for d in str(total))
+    
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    n = first_multiple_input[0]
+    k = int(first_multiple_input[1])
+    result = superDigit(n, k)
+    fptr.write(str(result) + '\n')
+    fptr.close()
+
+# Insertion Sort - Part 1
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+def insertionSort1(n, arr):
+    value = arr[-1]
+    index = n - 2
+    
+    while index >= 0 and arr[index] > value:
+        arr[index + 1] = arr[index]
+        print(' '.join(map(str, arr)))
+        index -= 1
+        
+    arr[index + 1] = value
+    print(' '.join(map(str, arr)))
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    insertionSort1(n, arr)
+
+# Insertion Sort - Part 2
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'insertionSort2' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+def insertionSort2(n, arr):
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        print(' '.join(map(str, arr)))
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    insertionSort2(n, arr)
+
+# Birthday Cake Candles
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+def birthdayCakeCandles(candles):
+    max_height = max(candles)
+    return candles.count(max_height)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    candles_count = int(input().strip())
+    candles = list(map(int, input().rstrip().split()))
+    result = birthdayCakeCandles(candles)
+    fptr.write(str(result) + '\n')
+    fptr.close()
+
